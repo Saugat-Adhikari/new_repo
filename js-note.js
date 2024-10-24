6b0a598cfd07c92fafa354a7b05fa9bbf3bee03d
Saugat Adhikari <cyberbits.predator@gmail.com>
2024-10-24 09:12:07 +0545

    some discription about the comparision in c and js about the variable

--- js-note.js
+++ js-note.js
@@ -1,1 +1,20 @@
 console.log(" this is the note of js ");
+//variable banauni tarika comparae wit c inn js
+/**
+ * in c 
+ * data_type variable_name = value ;
+ * int age = 20;
+ * 
+ * in js 
+ * let variable_name = value 
+ * let age = 20
+ * 
+ * the only difference while creating the variable in c and js is dollar($) can be used as a special character 
+ * let $name = "saugat"      ----> valid variable in js but not in c
+ * 
+ * 
+ * in c we have to write the exact data type of data while creating the variable but in js we can use let every where
+ *  const , int , float , char , double etc in c
+ *  let , const in js  
+*/
+
